4089ecb9878de48f430649034989acbfec51910d
Charlene Reilly <charlener83@hotmail.com>
2018-05-22 22:38:11 +0100

    Added Register form

---
+++ AdancedProgrammingCA/Register.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace AdancedProgrammingCA
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFname = new System.Windows.Forms.Label();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnRegForm = new System.Windows.Forms.Button();
+            this.lnkLogin = new System.Windows.Forms.LinkLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnCancelReg = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFname
+            // 
+            this.lblFname.AutoSize = true;
+            this.lblFname.Location = new System.Drawing.Point(51, 49);
+            this.lblFname.Name = "lblFname";
+            this.lblFname.Size = new System.Drawing.Size(80, 20);
+            this.lblFname.TabIndex = 0;
+            this.lblFname.Text = "Firstname";
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Location = new System.Drawing.Point(51, 94);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(74, 20);
+            this.lblSurname.TabIndex = 1;
+            this.lblSurname.Text = "Surname";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(48, 146);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(83, 20);
+            this.lblUsername.TabIndex = 2;
+            this.lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(51, 198);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(78, 20);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password";
+            // 
+            // btnRegForm
+            // 
+            this.btnRegForm.Location = new System.Drawing.Point(101, 258);
+            this.btnRegForm.Name = "btnRegForm";
+            this.btnRegForm.Size = new System.Drawing.Size(142, 54);
+            this.btnRegForm.TabIndex = 6;
+            this.btnRegForm.Text = "Register";
+            this.btnRegForm.UseVisualStyleBackColor = true;
+            // 
+            // lnkLogin
+            // 
+            this.lnkLogin.AutoSize = true;
+            this.lnkLogin.Location = new System.Drawing.Point(134, 348);
+            this.lnkLogin.Name = "lnkLogin";
+            this.lnkLogin.Size = new System.Drawing.Size(235, 20);
+            this.lnkLogin.TabIndex = 7;
+            this.lnkLogin.TabStop = true;
+            this.lnkLogin.Text = "Already Registered? Login Here";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(187, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 26);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(187, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(239, 26);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(187, 143);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(239, 26);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(187, 195);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(239, 26);
+            this.textBox4.TabIndex = 11;
+            // 
+            // btnCancelReg
+            // 
+            this.btnCancelReg.Location = new System.Drawing.Point(284, 258);
+            this.btnCancelReg.Name = "btnCancelReg";
+            this.btnCancelReg.Size = new System.Drawing.Size(142, 54);
+            this.btnCancelReg.TabIndex = 12;
+            this.btnCancelReg.Text = "Cancel";
+            this.btnCancelReg.UseVisualStyleBackColor = true;
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 415);
+            this.Controls.Add(this.btnCancelReg);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lnkLogin);
+            this.Controls.Add(this.btnRegForm);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.lblFname);
+            this.Name = "Register";
+            this.Text = "Register";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFname;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnRegForm;
+        private System.Windows.Forms.LinkLabel lnkLogin;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btnCancelReg;
+    }
+}
